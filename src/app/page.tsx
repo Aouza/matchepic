96ae7238ad258aa7190577162c439defb7b7307a
Alison <alison-cruz@hotmail.com>
2025-05-15 22:26:42 -0300

    style: update global styles and main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
+import Results from '../components/Results';
 import HowItWorks from '../components/HowItWorks';
 import CTA from '../components/CTA';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-[#0A0B14]">
       <Hero />
       <Features />
+      <Results />
       <HowItWorks />
       <CTA />
     </main>
